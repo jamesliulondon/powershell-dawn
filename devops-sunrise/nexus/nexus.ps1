cbfde81ab7a6192d643b31a068507e6434866fb9
root <root@localhost.localdomain>
2017-02-21 14:53:51 +0000

    added tentacle scripts

--- devops-sunrise/nexus/nexus.ps1
+++ devops-sunrise/nexus/nexus.ps1
@@ -1,5 +1,6 @@
 # Configuration
 $deployDir = "D:\Deploy"
+$nexus_data_root = "D:\nexus3data"
 $program_file_root = "C:\Program Files"
 $7zipExe = "C:\Program Files\7-Zip\7z.exe"
 $chocoUri = "https://chocolatey.org/install.ps1"
@@ -8,21 +9,24 @@ $chocoUri = "https://chocolatey.org/install.ps1"
 
 ## Nexus
 $nexus_data_backup_url = "https://s3-eu-west-1.amazonaws.com/insight-devops/Nexus/Data.zip"
-$nexus_msi_url = "http://download.sonatype.com/nexus/3/nexus-3.0.1-01-win64.zip"
-$nexus_config_location = "https://s3-eu-west-1.amazonaws.com/insight-devops/Nexus/nexus.vmoptions.j2"
-$nexus_data_root = "D:\nexus3data"
+$nexus_zip_url = "http://download.sonatype.com/nexus/3/nexus-3.0.1-01-win64.zip"
+$nexus_config_location = "https://s3-eu-west-1.amazonaws.com/insight-devops/Nexus/nexus.vmoptions"
+
 $nexus_program_folder = "Nexus"
 $nexus_data_directory = "sonatype-work"
 
-$nexus_zip_file = %{ $nexus_msi_url.split('/')[-1] }
+$nexus_zip_file = %{ $nexus_zip_url.split('/')[-1] }
 $nexus_data_backup_file = %{ $nexus_data_backup_url.split('/')[-1] }
 
 $nexus_zip_path = $deployDir + "\"  + $nexus_zip_file
 $nexus_data_backup_path = $deployDir + "\"  + $nexus_data_backup_file
+
 $nexus_program_folder_path = $program_file_root + "\" + $nexus_program_folder
+
 $nexus_config_path = $nexus_program_folder_path + "\bin"
 $nexus_binary = $nexus_config_path + "\nexus.exe"
 $nexus_data_path = $nexus_data_root + "\" + $nexus_data_directory
+
 
 echo "### Starting... ###"
 clear
@@ -39,26 +43,29 @@ echo "### Create Data directories ###"
 mkdir $nexus_data_path -force | Out-Null
 $clnt = new-object System.Net.WebClient
 
-echo "### Download Nexus ZIP ###"
-$clnt.DownloadFile($nexus_data_backup_url, $nexus_msi_path)
+echo "### Download Nexus Binaries ###"
+$clnt.DownloadFile($nexus_zip_url, $nexus_zip_path) | Out-Null
 
-echo "### Download Artifact Archive ###"
-$clnt.DownloadFile($nexus_msi_url, $nexus_data_backup_path)
+echo "### Download Nexus ZIP ###"
+$clnt.DownloadFile($nexus_data_backup_url, $nexus_data_backup_path) | Out-Null
 
 echo "### Choco Install ###"
-iwr $chocoUri -UseBasicParsing | iex
+iwr $chocoUri -UseBasicParsing | iex | Out-Null
 choco install -r -y 7zip | Out-Null
 
 echo "### Unzip Nexus ###"
-iex "& '$7zipExe' 'x' '$nexus_zip_path' '-aoa' '-o$nexus_program_folder_path'" | Out-Null
+iex "& '$7zipExe' 'x' '$nexus_zip_path' '-aoa' '-o$program_file_root'" | Out-Null
+
+
+
 
 echo "### Download Nexus Configuration ###"
-$clnt.DownloadFile($nexus_config_location, $nexus_config_path)
+$clnt.DownloadFile($nexus_config_location, $nexus_config_path) | Out-Null
 
 echo "### Unzip Nexus Data ###"
-iex "& '$7zipExe' 'x' '$nexus_data_backup_path' '-aoa' '-o$nexus_data_path'" | Out-Null
+#iex "& '$7zipExe' 'x' '$nexus_data_backup_path' '-aoa' '-o$nexus_data_path'" | Out-Null
 
 echo "### Installs Service ###"
-iex "& '$nexus_binary' '/install'"
+#iex "& '$nexus_binary' '/install'"
 
 echo "### The End ###"
---
+++ devops-sunrise/octopus/tentacle.ps1
@@ -0,0 +1,90 @@
+# Configuration
+$deployDir = "D:\Deploy"
+$7zipExe = "C:\Program Files\7-Zip\7z.exe"
+$chocoUri = "https://chocolatey.org/install.ps1"
+
+## Tentacle
+$tentacleUrl = "https://download.octopusdeploy.com/octopus/Octopus.Tentacle.3.10.1-x64.msi"
+$master_key = 'AaHLtYfmrfeXv126g5MEKw=='
+$tentacle_exe_path = 'C:\Program Files\Octopus Deploy\Tentacle\Tentacle.exe'
+
+
+$tentacle_file = %{ $tentacleUrl.split('/')[-1] }
+
+$tentacle_install_path = $deployDir + "\"  + $tentacle_file
+
+
+echo "### Starting... ###"
+clear
+
+echo "### Create deploy directories ###"
+mkdir $deployDir -force | Out-Null
+$clnt = new-object System.Net.WebClient
+
+
+echo "### Download Tentacle MSI ###"
+$clnt.DownloadFile($tentacleUrl, $tentacle_install_path)
+
+
+echo "### Choco Install ###"
+iwr $chocoUri -UseBasicParsing | iex
+choco install -r -y 7zip | Out-Null
+
+echo "### Install Octopus MSI ###"
+iex "& 'msiexec.exe' '/i' '$tentacle_install_path' '/passive' '/qr' "| Out-Null
+break
+
+echo "### Install Octopus Configure ###"
+$octopus_configure_instance = @'
+& $tentacle_exe_path create-instance --instance "Tentacle" --config "D:\Octopus\Tentacle.config" --console
+'@
+$octopus_configure_newcert = @'
+& $tentacle_exe_path new-certificate --instance "Tentacle" --if-blank --console
+'@
+$octopus_configure_resettrusts = @'
+& $tentacle_exe_pathconfigure --instance "Tentacle" --reset-trust --console
+'@
+$octopus_define_port = @'
+& $tentacle_exe_path configure --instance "Tentacle" --home "D:\Octopus" --app "D:\Octopus\Applications" --port "10933" --console
+'@
+$octopus_configure_newtrusts = @'
+& $tentacle_exe_path configure --instance "Tentacle" --trust "D002EA368D87032D0E071A7A385B0EA5CD375388 --console
+'@
+$octopus_reconfigure_firewall = @'
+"netsh" advfirewall firewall add rule "name=Octopus Deploy Tentacle" dir=in action=allow protocol=TCP localport=10933
+'@
+$octopus_register_apikey = @'
+& $tentacle_exe_path register-with --instance "Tentacle" --server "http://YOUR_OCTOPUS" --apiKey="API-YOUR_API_KEY" --role "web-server" --environment "Staging" --comms-style TentaclePassive --console
+'@
+$octopus_startconsole = @'
+& $tentacle_exe_path service --instance "Tentacle" --install --start --console
+'@
+
+start-sleep 10
+
+echo "- create-instance"
+iex $octopus_configure_instance | Out-Null
+echo "- create-newcerts"
+iex $octopus_configure_newcert  | Out-Null
+echo "- create-resettrusts"
+iex $octopus_configure_resettrusts  | Out-Null
+echo "- openport"
+iex $octopus_define_port  | Out-Null
+echo "- create-newtrust"
+iex $octopus_configure_newtrusts  | Out-Null
+echo "- create-SA-password"
+iex $octopus_reconfigure_firewall  | Out-Null
+echo "- reconfigure-firewall"
+iex $octopus_register_apikey  | Out-Null
+echo "- create-masterkey"
+iex $octopus_startconsole  | Out-Null
+
+
+
+Write-Host "Remember to restore the octopusdeploy database.":w
+
+#iex "& '$octopus_exe_path' 'service' '/install'"
+#iex "& '$octopus_exe_path' 'service' '/start'"
+
+
+echo "### The End ###"
